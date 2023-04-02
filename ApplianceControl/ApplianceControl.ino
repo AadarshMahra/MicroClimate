f04989cc1516e7546948eec87c03571d434ab1f2
Smit Purohit <purohitsmit@hotmail.com>
2023-04-02 15:49:16 -0500

    Broker Connection Issues

--- ApplianceControl/ApplianceControl.ino
+++ ApplianceControl/ApplianceControl.ino
@@ -1,10 +1,13 @@
-//Appliance Control Node Code Version 2
+//Appliance Control Node Code Version 3
 //v1: Read On/Off signal from Serial Monitor (Ex, Dx) from x = 0-3 
-//v2: Connect to broker
+//v2: Connect to broker 
+//v3: Use TinyMQTT to create client
 
-#include <ArduinoMqttClient.h>
+
 #include <WiFi.h>
-IPAddress ip(192, 168, 1, 119);
+#include "TinyMqtt.h"    // https://github.com/hsaturn/TinyMqtt
+#include "TinyStreaming.h" // https://github.com/hsaturn/TinyConsole
+
 const unsigned int NUM_RELAYS = 4;
 const char ENABLE = 'E';
 const char DISABLE = 'D';
@@ -12,20 +15,25 @@ const char DISABLE = 'D';
 unsigned int relay_on[NUM_RELAYS];
 const unsigned int relay_map[NUM_RELAYS] = {22,23,21,19};
 
-char ssid[] = "jeefsucksballs";    // AP SSID
-char pass[] = "trees12345";    // AP Password
+const char ssid[] = "MicroClimate";    // AP SSID
+const char pass[] = "MicroClimate";    // AP Password
 
 WiFiClient wifiClient;
-MqttClient mqttClient(wifiClient);
+MqttClient mqttClient;
 
-const char broker[] = "0.0.0.0";
+const char broker[] = "192.168.1.119";
 int        port     = 1883;
+const char topic[] = "sensor/temperature";
 const char topic0[]  = "AppControl/Outlet0";
 const char topic1[]  = "AppControl/Outlet1";
 const char topic2[]  = "AppControl/Outlet2";
 const char topic3[]  = "AppControl/Outlet3";
 
+void onPublishTopic(const MqttClient* /* srce */, const Topic& topic, const char* payload, size_t /* length */)
+{ Serial << "--> Client received msg on topic " << topic.c_str() << ", " << payload << endl; }
+
 void setup() {
+  
   //Set up serial
   Serial.begin(115200);
 
@@ -41,6 +49,7 @@ void setup() {
   pinMode(19,OUTPUT);
 
   // attempt to connect to WiFi network:
+  WiFi.mode(WIFI_STA);
   Serial.print("Attempting to connect to WPA SSID: ");
   Serial.println(ssid);
   while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
@@ -54,57 +63,49 @@ void setup() {
 
   // You can provide a unique client ID, if not set the library uses Arduino-millis()
   // Each client must have a unique client ID
-  // mqttClient.setId("clientId");
+  //mqttClient.setId("clientId");
 
   // You can provide a username and password for authentication
   // mqttClient.setUsernamePassword("username", "password");
 
   Serial.print("Attempting to connect to the MQTT broker: ");
-  Serial.println(ip);
+  Serial.println(broker);
 
-  if (!mqttClient.connect(ip, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-
-    while (1);
-  }
+  mqttClient.connect(broker, port);
 
   Serial.println("You're connected to the MQTT broker!");
   Serial.println();
 
   Serial.print("Subscribing to topic: ");
-  Serial.println(topic0);
+  Serial.println(topic);
   Serial.println();
 
   // subscribe to a topic
-  mqttClient.subscribe(topic0);
+  mqttClient.setCallback(onPublishTopic);
+  mqttClient.subscribe(topic);
 
   // topics can be unsubscribed using:
   // mqttClient.unsubscribe(topic);
 
   Serial.print("Waiting for messages on topic: ");
-  Serial.println(topic0);
+  Serial.println(topic);
   Serial.println();
   
 }
 
 void loop() {
-  int messageSize = mqttClient.parseMessage();
-  if (messageSize) {
-    // we received a message, print out the topic and contents
-    Serial.print("Received a message with topic '");
-    Serial.print(mqttClient.messageTopic());
-    Serial.print("', length ");
-    Serial.print(messageSize);
-    Serial.println(" bytes:");
+  mqttClient.loop();
+  static auto next_req = millis();
+  
+  if (millis() > next_req)
+  {
+    next_req += 1000;
 
-    // use the Stream interface to print the contents
-    while (mqttClient.available()) {
-      Serial.print((char)mqttClient.read());
+    if (not mqttClient.connected())
+    {
+      Serial << millis() << ": Not connected to broker" << endl;
+      return;
     }
-    Serial.println();
-
-    Serial.println();
   }
     /*
   //Get Command if there is one (all commands are 2 bytes long: E(nable)/D(isable) and x)
