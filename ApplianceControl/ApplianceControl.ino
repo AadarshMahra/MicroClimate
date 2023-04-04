552e5f2487cc7af7c360c2732427735b88f12c72
Smit Purohit <purohitsmit@hotmail.com>
2023-04-04 15:16:58 -0500

    Removed commented out code

--- ApplianceControl/ApplianceControl.ino
+++ ApplianceControl/ApplianceControl.ino
@@ -132,36 +132,6 @@ void loop() {
       client.publish(outlet_status[i], relay_status);
     }
   }
-    /*
-  //Get Command if there is one (all commands are 2 bytes long: E(nable)/D(isable) and x)
-  static char command;
-  static unsigned int relay_id;
-  if (Serial.available() >= 2) {   // check if at least 2 bytes are available
-    Serial.println(Serial.available());
-    char data[2];   // create an array to store the data
-    Serial.readBytes(data, 2);   // read 2 bytes from the serial monitor and store them in the array
-    Serial.read();
-    command = data[0];
-    relay_id = data[1] - 48; //Subtract 48 to switch from char to int, could add a parseInt but it messes stuff up
-    Serial.print(command);   // print the value to the serial monitor
-    Serial.println(relay_id);   // print the value to the serial monitor
-
-    //Update array
-    if(command == ENABLE){
-      Serial.print("Turning ON Relay ");
-      Serial.println(relay_id);
-      
-      relay_on[relay_id] = 1;
-      digitalWrite(relay_map[relay_id],HIGH);
-    }
-    else if(command == DISABLE){
-      Serial.print("Turning OFF Relay ");
-      Serial.println(relay_id);
-      
-      relay_on[relay_id] = 0;
-      digitalWrite(relay_map[relay_id],LOW);
-    }
-  }*/
   for(int i = 0; i < NUM_RELAYS; ++i){
     Serial.print(relay_on[i]);
   }
